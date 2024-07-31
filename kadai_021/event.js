29adbb12f49a7fa7e3a572cbd84018c9e87c423a
yujihorikawa <lucky.boy.ikki@gmail.com>
2024-07-31 16:54:46 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+const btn=document.getElementById('btn');
+
+
+btn.addEventListener('click',()=>{
+  
+  setTimeout(() => {
+    const text=document.getElementById('text'); 
+ 
+  text.textContent='ボタンをクリックしました';
+}, 2000);
+});
